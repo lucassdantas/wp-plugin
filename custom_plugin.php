6f23972e54e9b064e13c2dac662017bf5ee75a68
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-06-24 07:03:27 -0300

    firstcommit

---
+++ custom_plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: custom_plugin
+* Plugin URI: https://github.com/lucassdantas
+* Description: test plugin
+* Version: 0.1
+* Author: Lucas Dantas 
+* Author URI: linkedin.com/in/lucas-de-sousa-dantas/
+**/
