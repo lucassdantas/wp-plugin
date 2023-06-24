395bd4d1b97b57b5681f80c8b2917059ba1655e7
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-06-24 07:14:05 -0300

    add new section under wc

--- custom_plugin.php
+++ custom_plugin.php
@@ -1,9 +1,21 @@
 <?php
 /**
-* Plugin Name: custom_plugin
+* Plugin Name: custom plugin
 * Plugin URI: https://github.com/lucassdantas
 * Description: test plugin
 * Version: 0.1
 * Author: Lucas Dantas 
 * Author URI: linkedin.com/in/lucas-de-sousa-dantas/
 **/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+add_filter( 'woocommerce_get_sections_products', 'wcslider_add_section' );
+function wcslider_add_section( $sections ) {
+	
+	$sections['wcslider'] = __( 'WC Slider', 'text-domain' );
+	return $sections;
+	
+}
